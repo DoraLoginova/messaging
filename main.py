d6d47a37e2530fb745182e75c086401fa766e7c5
Dora Loginova <doris-i@mail.ru>
2024-10-17 18:59:28 +0300

    change_main.py

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from .auth import register_user, login_user
+from .servicedb import connect_db
 
 app = FastAPI()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@app.post("/register/")
+async def register(username, password, db: Session = Depends(connect_db)):
+    result = await register_user(username, password, db)
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Пользователь уже существует"
+        )
+    return {"detail": "Пользователь успешно зарегистрирован"}
+
+
+@app.post("/token")
+async def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(connect_db)
+):
+    return await login_user(db, form_data.username, form_data.password)
+
+
 @app.post("/messages/")
-async def send_message(message: Message, db: Session = Depends(get_db)):
+async def send_message(message: Message, db: Session = Depends(connect_db)):
     # Логика для отправки сообщения
     pass
 
-@app.websocket("/ws/{user_id}")
-async def websocket_endpoint(websocket: WebSocket, user_id: int):
-    # Логика для обработки WebSocket соединений
-    pass
-
-
 @app.get("/messages/history/{user_id}")
-def get_history(user_id: int, db: Session = Depends(get_db)):
+async def get_history(user_id: int, db: Session = Depends(get_db)):
     # Логика для извлечения истории сообщений
     pass
+
---
+++ servicedb.py
@@ -0,0 +1,50 @@
+import os
+import asyncio
+import aiopg
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from dotenv import load_dotenv
+
+from .models import Base
+
+load_dotenv()
+
+DB = os.getenv("DB_URL")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
+
+engine = create_engine(DB, pool_size=10, max_overflow=20)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+async def wait_for_db():
+    for _ in range(5):
+        try:
+            async with aiopg.connect(
+                dbname=DB_NAME,
+                user=DB_USER,
+                password=DB_PASSWORD,
+                host=DB_HOST,
+                port=DB_PORT
+            ):
+                print("БД готова")
+                return
+        except Exception as e:
+            print(f"Не удалось подключиться к БД: {e}")
+            await asyncio.sleep(5)
+    raise Exception("PostgreSQL не доступен.")
+
+
+async def connect_db() -> Session:
+    await wait_for_db()
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+Base.metadata.create_all(bind=engine)
